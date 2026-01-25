dc311b4cb08c475e0150d9d4198a0d3cecf625cb
prostogma <fomin31415@gmail.com>
2026-01-25 16:07:52 +0300

    rename users-services to users_services

--- users-services/app/db/base.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.ext.asyncio import AsyncAttrs
-
-
-# Базовый класс для всех моделей
-class Base(AsyncAttrs, DeclarativeBase):
-    __abstract__ = (
-        True  # Класс абстрактный, чтобы не создавать под него отдельную таблицу
-    )
